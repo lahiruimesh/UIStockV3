b4057cbccba063f6729c65a99b2596cbae339726
lahiru imesh <imeshbl.22@uom.lk>
2025-05-31 11:08:06 +0530

    Made some changes

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env variables
+dotenv.config();
+
+// Connect to DB
+connectDB();
+
+// Init app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
