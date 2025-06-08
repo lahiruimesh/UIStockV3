771ecf5437d663bda7b75a75c093029bbd2df888
lahiru imesh <imeshbl.22@uom.lk>
2025-06-08 21:00:12 +0530

    Add styles for login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -36,17 +37,29 @@ const Login = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 bg-purple-500">
-    <div className="flex bg-gray-500 flex-col">
-      <h1>Login</h1>
-      <p>Login to your account to continue</p>
+    <div
+    className="w-screen h-screen bg-cover grid grid-cols-2 bg-center flex items-center justify-center"
+    style={{ backgroundImage: `url('/hero.png')` }}
+    >
+    <div>
+    <img
+          src="/image2.png"
+          alt="Hero Graphic"
+          className="max-w-[600px] h-[480px] mr-[220px] mt-[70px]"
+        />
     </div>
-    <div className="bg-blue-500">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-sm mx-auto mt-10">
-        <input name="email" type="email" placeholder="Email" onChange={handleChange} className="p-2 border" />
-        <input name="password" type="password" placeholder="Password" onChange={handleChange} className="p-2 border" />
-        <button type="submit" className="bg-green-600 text-white p-2 rounded">Login</button>
+    <div className="mr-6">
+      <div className="text-right flex flex-col items-end mr-24 pt-4">
+        <h1 className="text-4xl text-white font-bold">Welcome Back<br />Sign In to Your Account</h1>
+      </div>
+      <div className="flex justify-end px-8 mr-16">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-sm mt-10 w-full">
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} className="p-2 border h-14 opacity-50 rounded"  />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} className="p-2 border h-14 opacity-50 rounded" />
+        <button type="submit" className="bg-blue-600 h-12 text-white p-2 rounded">Login</button>
+        <p className="text-white text-center">Don’t I have account? <Link to = "/register" className="text-blue-600">Register</Link></p>
       </form>
+      </div>
     </div>
     </div>
   );
